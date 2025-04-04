a7cbf84e22cce4845b0f597f7a60400981448431
WEY <stefan.fire@gmail.com>
2025-04-04 10:18:18 -0300

    Initial Commit

---
+++ LastTry/LastTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastTryApp.swift
+//  LastTry
+//
+//  Created by Stefan Wey on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct LastTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
